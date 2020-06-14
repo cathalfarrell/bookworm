9fb15c7ae716bdd3b8cacb91d3d91d578656c3d1
Cathal Farrell <cathalproductions@gmail.com>
2020-06-14 12:57:22 +0100

    Accessibility added

--- Bookworm/RatingView.swift
+++ Bookworm/RatingView.swift
@@ -26,7 +26,6 @@ struct RatingView: View {
             if label.isEmpty == false {
                 Text(label)
             }
-
             ForEach(1..<maximumRating + 1) { number in
                 self.image(for: number)
                     .foregroundColor(number > self.rating ? self.offColor : self.onColor)
@@ -34,6 +33,16 @@ struct RatingView: View {
                         print("Tap this.")
                         self.rating = number
                     }
+                    // Accessibility
+                    .accessibility(label: Text("\(number == 1 ? "1 star" : "\(number) stars")"))
+                    .accessibility(removeTraits: .isImage) //no need to say its an image
+                    /*
+                     Tell the system that each star is actually a button, so users know it can be tapped.
+                     While we’re here, we can make VoiceOver do an even better job by adding a second trait,
+                     .isSelected, if the star is already highlighted.
+                     */
+                    .accessibility(addTraits: number > self.rating ? .isButton : [.isButton, .isSelected])
+
             }
         }
     }
